22999726aaadf8ed58eaebe974bc63a082af77ad
Quảng Ngô <quangngo-13@users.noreply.github.com>
2020-06-30 11:44:12 +0700

    first commit

---
+++ next-js-project/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>SEO</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <div>Xin chào1 1</div>
+      </main>
+    </div>
+  )
+}
---
+++ next-js-project/pages/user.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const user = () => {
+    return (
+        <div>            
+            Xin chào user
+        </div>
+    );
+};
+
+export default user;
